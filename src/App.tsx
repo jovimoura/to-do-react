b358ff4004e6b5650a0787083a8f24806ced907d
jovimoura <joaovictormoura@poli.ufrj.br>
2021-10-05 13:41:31 -0300

    finalizando estilos e parte do react

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import * as C from './App.styles' //Importando tudo como C de app.styles
+import { Item } from './types/item';
+import { ListItem } from './components/ListItem';
 
-function App() {
+const App = () => {
+  const [list, setList] = useState<Item[]>([
+    {id: 1, name: 'comprar pão', done: false},
+    {id: 2, name: 'comprar carro', done: false}
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Area>
+        <C.Header>Lista de Tarefas</C.Header>
+
+        {/*área de add nova tarefa*/}
+
+        {list.map((item,index)=>(
+          <ListItem key={index} item={item} /> 
+        ))}
+      </C.Area>
+    </C.Container>
   );
 }
 
---
+++ src/components/ListItem/index.tsx
@@ -0,0 +1,15 @@
+import * as C from "./styles"
+import { Item } from "../../types/item"
+
+type Props = {
+    item: Item
+}
+
+export const ListItem = ({item}:Props) => {
+    return (
+        <C.Container>
+            <input type="checkbox"/>
+            <label>{item.name}</label>
+        </C.Container>
+    )
+}
